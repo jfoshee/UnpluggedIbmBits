af99e3c7d77dac0056abed5d8efcc9b854ff5c22
Jacob Foshee <dev@unpluggeddevelopment.com>
2013-01-19 17:50:43 -0800

    BinaryWriter.WriteEbcdic(string)

---
+++ Unplugged.IbmBits.Tests/BinaryWriterExtensionMethodsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unplugged.IbmBits.Tests
+{
+    [TestClass]
+    public class BinaryWriterExtensionMethodsTest
+    {
+        #region WriteStringEbcdic
+
+        [TestMethod]
+        public void WriteEbcdicShouldWriteConvertedBytes()
+        {
+            var value = "Nope";
+            var expected = new byte[] { 0xD5, 0x96, 0x97, 0x85 };
+            VerifyBytesWritten(w => w.WriteEbcdic(value), expected);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void ShouldThrowWhenArgumentNullForWriteEbcdic()
+        {
+            (null as BinaryWriter).WriteEbcdic(" ");
+        }
+
+        #endregion
+
+        public static void VerifyBytesWritten(Action<BinaryWriter> act, byte[] expected)
+        {
+            var bytes = new byte[expected.Length];
+            using (var stream = new MemoryStream(bytes))
+            using (var writer = new BinaryWriter(stream))
+            {
+                // Act
+                act(writer);
+
+                // Assert
+                stream.Position.Should().Be(expected.Length, "Wrong number of bytes were written.");
+            }
+            bytes.Should().Equal(expected);
+        }
+
+    }
+}
---
+++ Unplugged.IbmBits/BinaryWriterExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Unplugged.IbmBits
+{
+    public static class BinaryWriterExtensionMethods
+    {
+        /// <summary>
+        /// Writes the given Unicode string as an 8-bit EBCDIC encoded character string
+        /// </summary>
+        public static void WriteEbcdic(this BinaryWriter writer, string value)
+        {
+            if (ReferenceEquals(null, writer))
+                throw new ArgumentNullException("writer");
+            var bytes = IbmConverter.GetBytes(value);
+            writer.Write(bytes);
+        }
+    }
+}
